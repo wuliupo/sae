dc8c3b8af6ae5f18aed168c3528d3233cf757427
Liupo Wu <wuliupo@163.com>
2015-02-01 14:11:48 +0800

    after install

--- emulation/mysqlconf/mysql.php
+++ emulation/mysqlconf/mysql.php
@@ -1,12 +1,12 @@
 <?php
-define('SAE_MYSQL_USER', '');
+define('SAE_MYSQL_USER', 'root');
 define('SAE_MYSQL_PASS', '');
 define('SAE_MYSQL_HOST_M', '127.0.0.1');
 define('SAE_MYSQL_HOST_S', '127.0.0.1');
 define('SAE_MYSQL_PORT', '3306');
 
 // document root
-$_SERVER['DOCUMENT_ROOT'] = 'F:\windows\wwwroot' ;
+$_SERVER['DOCUMENT_ROOT'] = 'C:\dev\sae\wwwroot' ;
 
 define('HTTP_PORT','80') ;
 define('HTTPS_PORT','443') ;
--- emulation/password.cmd
+++ emulation/password.cmd
@@ -1,4 +1,4 @@
 @echo off
->"%temp%\in.com" echo hP1X500P[PZBBBfh#b##fXf-V@`$fPf]f3/f1/5++u5x
-for /f "tokens=*" %%a in ('"%temp%\in.com"') do (  set "InputPass=%%a" )
-echo %InputPass%
+set SAE_PATH=%~dp0
+"%%SAE_PATH%..\bin\other\editv22\EditV64.exe" -m pass
+echo %pass%
